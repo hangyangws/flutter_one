696d396d91de66860cf904ecaa19a7471cf70a93
hangyangws <hangyangws@qq.com>
2019-06-19 10:17:29 +0800

    init

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+// 必须取名为 main
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Welcome to Flutter1',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Welcome to Flutter2'),
+        ),
+        body: Center(
+          child: Text('Hello Wdorld'),
+        ),
+      ),
+    );
+  }
+}
